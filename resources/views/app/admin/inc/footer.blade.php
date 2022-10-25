9f86082838da7b07cca2bdeca9d611be4d2811c4
ahevr <ahmethusreversen@hotmail.com>
2022-10-25 16:51:01 +0300

    sidebar düzenlendi

--- resources/views/app/admin/inc/footer.blade.php
+++ resources/views/app/admin/inc/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>2021 &copy; Mazer</p>
+            <p>{{date("Y")}} &copy; egesedefavize</p>
         </div>
         <div class="float-end">
             <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                    href="https://saugi.me">Saugi</a></p>
+                    href="https://aheworks.com">ahevr</a></p>
         </div>
     </div>
 </footer>
--- resources/views/app/admin/inc/script.blade.php
+++ resources/views/app/admin/inc/script.blade.php
@@ -1,2 +1,3 @@
 <script src="{{asset("build/tema/admin/dist/")}}/assets/js/bootstrap.js"></script>
 <script src="{{asset("build/tema/admin/dist/")}}/assets/js/app.js"></script>
+<script src="https://cdn.lordicon.com/qjzruarw.js"></script>
